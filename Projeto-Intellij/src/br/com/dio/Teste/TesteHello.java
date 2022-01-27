bb28823a892049b009c758ab8bd8cb99cc076cd7
Zukamia <luis_aqw@hotmail.com>
2022-01-27 15:04:40 -0400

    Update TesteHello.java

--- Projeto-Intellij/src/br/com/dio/Teste/TesteHello.java
+++ Projeto-Intellij/src/br/com/dio/Teste/TesteHello.java
@@ -1,2 +1,7 @@
-package br.com.dio.Teste;public class TesteHello {
+package br.com.dio.Teste;
+
+public class TesteHello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
